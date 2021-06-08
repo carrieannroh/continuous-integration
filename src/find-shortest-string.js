b717eb11c56b336d8fe7c82f08ee01b10484a4d0
Carrie <carriearoberts95@gmail.com>
2021-06-08 15:42:04 +0100

    committing my fix to find-shortest-string.js

--- src/find-shortest-string.js
+++ src/find-shortest-string.js
@@ -8,9 +8,9 @@
  * @returns {string} - the shortest string
  */
 function findShortestString(stringArr) {
-  const shortestString = stringArr[0];
+  let shortestString = stringArr[0];
   for (let str of stringArr) {
-    if ((str.length = shortestString.length)) {
+    if ((str.length > shortestString.length)) {
       shortestString = str;
     }
   }
